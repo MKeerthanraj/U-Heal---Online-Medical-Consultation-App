8ce23865ebaf27375bbd6cb815e3c1c684d548d4
Mansi-Balaganur <73600989+Mansi-Balaganur@users.noreply.github.com>
2022-02-13 17:53:23 +0530

    Add files via upload

---
+++ SpecArrayListClass.java
@@ -0,0 +1,50 @@
+package com.example.medicalconsultation;
+
+import java.util.ArrayList;
+
+public class SpecArrayListClass {
+    ArrayList<String> specList;
+    public void setSpecList() {
+        specList=new ArrayList<String>();
+        specList.add("Select");
+        specList.add("Allergist");
+        specList.add("Anesthesiologist");
+        specList.add("Cardiology");
+        specList.add("Colon and Rectal Surgeon");
+        specList.add("Critical Care Medicine Specialist");
+        specList.add("Dermatologist");
+        specList.add("Endocrinologist");
+        specList.add("Ear Nose Throat");
+        specList.add("Emergency Medicine Specialist");
+        specList.add("Family Physician");
+        specList.add("Gastroenterologist");
+        specList.add("General Surgery");
+        specList.add("Gynecology");
+        specList.add("Hematologist");
+        specList.add("Infectious Disease Specialist");
+        specList.add("Internal Medicine");
+        specList.add("Medical Geneticist");
+        specList.add("Nephrologist");
+        specList.add("Neurosurgery");
+        specList.add("Neurologist");
+        specList.add("Obstetricians and Gynecologist");
+        specList.add("Oncologist");
+        specList.add("Ophthalmologist");
+        specList.add("Orthopedics");
+        specList.add("Pathologist");
+        specList.add("Physiotherapy");
+        specList.add("Pediatrician");
+        specList.add("Psychiatry");
+        specList.add("Plastic Surgeon");
+        specList.add("Preventive Medicine Specialist");
+        specList.add("Radiologist");
+        specList.add("Rheumatologist");
+        specList.add("Sleep Medicine Specialist");
+        specList.add("General Surgeon");
+        specList.add("Urologist");
+    }
+
+    public ArrayList<String> getSpecList(){
+        return specList;
+    }
+}
